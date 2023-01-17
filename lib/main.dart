7dc7d07f0037f8b5ff8bc2432089d0769ce572b8
priyanka <bharadwajpriyanka204@gmail.com>
2023-01-17 03:22:09 -0800

    otp generator

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:otpgenerator/home.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
